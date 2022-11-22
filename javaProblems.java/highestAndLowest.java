b3a1f54c12cd31dc0b5deb8acccd79e6534d8062
Eugene Shin <eugeneshine1@gmail.com>
2022-11-22 12:33:24 -0500

    refactor for single testcase

--- javaProblems.java/highestAndLowest.java
+++ javaProblems.java/highestAndLowest.java
@@ -1,13 +1,16 @@
 class Kata {
     public static String highAndLow(String numbers) {
-        // if the length of number is 2, return the string
+        // if the length of string is 2, return both numbers in string format
+        // this is because if both numbers are the same, it needs to be returned separtely 
         // set variables for highest and lowest
         // loop throught the string and parseInt the see value of the numbers
         // compare if set number to a varible if it is the highest
         // do the same with the lowest number
         // return both numbers as a string 
         if (numbers.length()==2){       
-            return numbers;
+            int nums = Integer.parseInt(numbers);
+            
+            return String.format("%d %d", nums, nums);
           }
           int highest =-1;
           int lowest =1;
