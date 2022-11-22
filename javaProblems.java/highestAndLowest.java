9c5b16594267699903368dceebfdf1a8d9311e7d
Eugene Shin <eugeneshine1@gmail.com>
2022-11-22 12:24:18 -0500

    string format to return numbers into string

--- javaProblems.java/highestAndLowest.java
+++ javaProblems.java/highestAndLowest.java
@@ -6,6 +6,21 @@ class Kata {
         // compare if set number to a varible if it is the highest
         // do the same with the lowest number
         // return both numbers as a string 
-        
+        if (numbers.length()==2){       
+            return numbers;
+          }
+          int highest =-1;
+          int lowest =1;
+          String [] nm = numbers.split(" ");
+         
+          for (int x=0; x<nm.length;x++){
+            if(Integer.parseInt(nm[x])>=highest){
+              highest = Integer.parseInt(nm[x]);
+            }
+            if(Integer.parseInt(nm[x])<=lowest){
+              lowest = Integer.parseInt(nm[x]);
+            }
+          }
+          return String.format("%d %d", highest, lowest);
     }
 }
