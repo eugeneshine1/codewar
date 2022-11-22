f2f950c6b9b0fce3c6e8ddd2e58af3518b3b065e
Eugene Shin <eugeneshine1@gmail.com>
2022-11-22 12:22:36 -0500

    pseudo code

---
+++ javaProblems.java/highestAndLowest.java
@@ -0,0 +1,11 @@
+class Kata {
+    public static String highAndLow(String numbers) {
+        // if the length of number is 2, return the string
+        // set variables for highest and lowest
+        // loop throught the string and parseInt the see value of the numbers
+        // compare if set number to a varible if it is the highest
+        // do the same with the lowest number
+        // return both numbers as a string 
+        
+    }
+}
