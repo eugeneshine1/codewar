878778e256c9cc5e949667c4cd7799d9dcec40b6
Eugene Shin <eugeneshine1@gmail.com>
2022-11-23 10:15:57 -0500

    pseudo code

---
+++ javaProblems.java/complementaryDNA.java
@@ -0,0 +1,10 @@
+class DnaStrand {
+    public static String makeComplement(String dna) {
+      // instantiate an emtpy string
+      // loop through the string 
+      // for every A character add T string to the new instantiated string'
+      // if character is T add A, if C add G, and if G add C
+      // return the new string
+    }
+  }
+  
