06ff2873bcb94db9339850e6f9697a64f018976f
Eugene Shin <eugeneshine1@gmail.com>
2022-11-23 10:18:35 -0500

    charAt to look at characters in string

--- javaProblems.java/complementaryDNA.java
+++ javaProblems.java/complementaryDNA.java
@@ -5,6 +5,20 @@ class DnaStrand {
       // for every A character add T string to the new instantiated string'
       // if character is T add A, if C add G, and if G add C
       // return the new string
+
+        String answer="";
+        for (int x=0; x<dna.length(); x++) {
+            if (dna.charAt(x)=='A'){
+            answer += "T";
+            } else if (dna.charAt(x)=='T') {
+            answer += "A";
+            } else if (dna.charAt(x)=='C') {
+            answer += "G";
+            } else {
+            answer += "C";
+            }
+        }
+        return answer;
     }
   }
   
