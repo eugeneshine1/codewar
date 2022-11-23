80b5d7c356a10094aecefe4ea658a8ad6aa09ced
Eugene Shin <eugeneshine1@gmail.com>
2022-11-23 11:04:40 -0500

    add more pseudo code

--- countingSheep.java
+++ countingSheep.java
@@ -1,7 +1,9 @@
 class Counter {
     public int countSheeps(Boolean[] arrayOfSheeps) {
+        // try catch for null pointer exception
         // loop through the array
         // if the elements are true, add 1 to the count variable
+        // add the catch phrase here for any null values in the array
         // return the count
         int count =0;
         for (Boolean answer : arrayOfSheeps){
