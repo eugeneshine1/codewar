7c2bd86ace72d407bb77da7525a0f952678345eb
Eugene Shin <eugeneshine1@gmail.com>
2022-11-23 11:00:08 -0500

    pseudo code

---
+++ countingSheep.java
@@ -0,0 +1,7 @@
+class Counter {
+    public int countSheeps(Boolean[] arrayOfSheeps) {
+        // loop through the array
+        // if the elements are true, add 1 to the count variable
+        // return the count
+    }
+}
