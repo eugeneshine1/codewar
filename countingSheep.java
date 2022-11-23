3838217073ba8e41bc40530a001c90621ccba83b
Eugene Shin <eugeneshine1@gmail.com>
2022-11-23 11:00:51 -0500

    foreach loop

--- countingSheep.java
+++ countingSheep.java
@@ -3,5 +3,12 @@ class Counter {
         // loop through the array
         // if the elements are true, add 1 to the count variable
         // return the count
+        int count =0;
+        for (Boolean answer : arrayOfSheeps){
+            if (answer==true){
+              count++;
+            } 
+          }
+          return count;
     }
 }
