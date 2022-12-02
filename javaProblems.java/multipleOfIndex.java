ba91f25065fd618db20aecc2d2b1c747811764fd
Eugene Shin <eugeneshine1@gmail.com>
2022-12-02 11:44:33 -0500

    for loops

--- javaProblems.java/multipleOfIndex.java
+++ javaProblems.java/multipleOfIndex.java
@@ -5,5 +5,24 @@ class ZywOo {
       //create a new array with size of count
       //loop through the array and add each number taht was divisible by its own index
       //return the array
+      int k = 0;
+      int count = 0;
+          for (int i = 1; i < array.length; i++) {
+              if (array[i] % i == 0) {
+      
+                count++;
+              }
+          }
+      int nums[] = new int[count];
+       for (int i = 1; i < array.length; i++) {
+              if (array[i] % i == 0) {
+                    nums[k] = array[i];
+          
+                  k++;
+      }
+         }
+          return nums;
     }
+    
   }
+    
