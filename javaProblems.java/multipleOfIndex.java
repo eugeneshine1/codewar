238a77b13c1f8884eb845e0777247afa429b07e1
Eugene Shin <eugeneshine1@gmail.com>
2022-12-02 11:47:07 -0500

    redo pseudo code

--- javaProblems.java/multipleOfIndex.java
+++ javaProblems.java/multipleOfIndex.java
@@ -1,28 +1,10 @@
 class ZywOo {
     public static int[] multipleOfIndex(int[] array) {
+      //create a list
       //loop through the array and check if number in the array is divisible to its index
-      //add 1 to counter if true
-      //create a new array with size of count
-      //loop through the array and add each number taht was divisible by its own index
-      //return the array
-      int k = 0;
-      int count = 0;
-          for (int i = 1; i < array.length; i++) {
-              if (array[i] % i == 0) {
-      
-                count++;
-              }
-          }
-      int nums[] = new int[count];
-       for (int i = 1; i < array.length; i++) {
-              if (array[i] % i == 0) {
-                    nums[k] = array[i];
-          
-                  k++;
-      }
-         }
-          return nums;
+      //if so, add the number to the list
+      //return the list as an array using stream 
+     
     }
-    
   }
     
