7463af1d611e0adda46a0488cf81f0579317a06f
Eugene Shin <eugeneshine1@gmail.com>
2022-12-02 11:43:41 -0500

    pseudo code

---
+++ javaProblems.java/multipleOfIndex.java
@@ -0,0 +1,9 @@
+class ZywOo {
+    public static int[] multipleOfIndex(int[] array) {
+      //loop through the array and check if number in the array is divisible to its index
+      //add 1 to counter if true
+      //create a new array with size of count
+      //loop through the array and add each number taht was divisible by its own index
+      //return the array
+    }
+  }
