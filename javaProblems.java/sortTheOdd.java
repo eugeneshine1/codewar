2ab744404816e44a9a0e1205a32adfbd6dec3d0b
Eugene Shin <eugeneshine1@gmail.com>
2022-12-05 12:23:42 -0500

    pseudo code

---
+++ javaProblems.java/sortTheOdd.java
@@ -0,0 +1,9 @@
+class Kata {
+    public static int[] sortArray(int[] array) {
+        //use bubble sort
+        //double for loop
+        //if both the number in the array is odd and one is greater than the other, switch places
+        //use temp variable to hold the value when switching
+        //return array
+    }
+  }
