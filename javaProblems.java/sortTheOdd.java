d7856c6872722503e6f401b03a1f4aa9e7dfb8d3
Eugene Shin <eugeneshine1@gmail.com>
2022-12-05 12:24:17 -0500

    bubble sort (double for loop)

--- javaProblems.java/sortTheOdd.java
+++ javaProblems.java/sortTheOdd.java
@@ -5,5 +5,18 @@ class Kata {
         //if both the number in the array is odd and one is greater than the other, switch places
         //use temp variable to hold the value when switching
         //return array
+
+        for (int x=0; x<array.length;x++){
+            for (int y=0; y<array.length;y++){
+              if (array[y]%2 ==0 && x!=y){
+              } else if (array[y]%2 ==1 && array[x]%2 ==1 && x!=y && array[y]>array[x]){
+                int temp =array[y];
+                array[y]= array[x];
+                array[x] = temp;
+              }
+            }
+          }
+          return array;
+      
     }
   }
