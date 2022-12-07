ae6e7588dfae9256f79487fcd0e8009e8e54c4de
Eugene Shin <eugeneshine1@gmail.com>
2022-12-07 12:20:07 -0500

    pseudo code

---
+++ javaProblems.java/creditCardMask.java
@@ -0,0 +1,10 @@
+class Maskify {
+    public static String maskify(String str) {
+        //if the length of the string given is less or equal to 4, return the string
+        // create an emtpy string
+        // loop from 1 to the length-4 of the string
+        // with each iteration, add # to the empty string
+        // return the string variable with the # and the last for digits in the string
+         
+    }
+}
