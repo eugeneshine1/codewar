50c20e24a3c7185d1109c8d88895200c2e5b6c8a
Eugene Shin <eugeneshine1@gmail.com>
2022-12-07 12:26:09 -0500

    empty string created to add to it

--- javaProblems.java/creditCardMask.java
+++ javaProblems.java/creditCardMask.java
@@ -6,6 +6,13 @@ class Maskify {
         // with each iteration, add # to the empty string
         // return the string variable with the # and the last for digits in the string
         
-        
+        if (str.length()<=4) {
+            return str;
+        }
+        String ans="";
+        for (int i=0; i<str.length()-4;i++) {
+            ans+="#";
+        }
+        return ans + str.substring(str.length()-4, str.length()-1);
     }
 }
