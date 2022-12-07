48592b55dde3a6ff93842ad4a19428f54da72213
Eugene Shin <eugeneshine1@gmail.com>
2022-12-06 21:44:45 -0500

    charAt() to look at char in string

--- javaProblems.java/vowelCount.java
+++ javaProblems.java/vowelCount.java
@@ -3,5 +3,13 @@ class Vowels {
       // loop through the string and look at each character in the string
       // if the string is a vowel add 1 to the counter
       // return the count
+
+    int count =0;
+    for (int x =0; x<str.length();x++) {
+      if (str.charAt(x)== 'a' || str.charAt(x)== 'e'|| str.charAt(x)== 'i' ||str.charAt(x)== 'o'||str.charAt(x)== 'u'){
+        count++;
+      }
+    }
+    return count;
     }
   }
