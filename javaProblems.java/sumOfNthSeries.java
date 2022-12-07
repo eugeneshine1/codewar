44a4aa8aca49520137c1ec968c163cc5109819d1
Eugene Shin <eugeneshine1@gmail.com>
2022-12-06 21:52:25 -0500

    pseudo code

---
+++ javaProblems.java/sumOfNthSeries.java
@@ -0,0 +1,9 @@
+class NthSeries {
+	public static String seriesSum(int n) {
+		// loop from 1 to the given number n 
+        // each iteration, add 1 divide by the denominator to a sum variable
+		// before interating, increase the denominator by 3
+		// interate through till n times
+		// return the sum variable 
+	}
+}
