ee314af3f83cc98e8da56e1da786fc1482700ad2
Eugene Shin <eugeneshine1@gmail.com>
2022-12-06 21:43:35 -0500

    pseudo code

---
+++ javaProblems.java/vowelCount.java
@@ -0,0 +1,7 @@
+class Vowels {
+    public static int getCount(String str) {
+      // loop through the string and look at each character in the string
+      // if the string is a vowel add 1 to the counter
+      // return the count
+    }
+  }
