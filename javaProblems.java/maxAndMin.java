c9df64c7ef6af24e3a44c91764b26c59ffff9303
Eugene Shin <eugeneshine1@gmail.com>
2022-12-12 10:52:54 -0500

    setting variable to lesser value

--- javaProblems.java/maxAndMin.java
+++ javaProblems.java/maxAndMin.java
@@ -4,7 +4,13 @@ class Kata {
         //loop through the array and compare it to the intialized variable 
         //if its less, set the variable equal to that number
         //at the end of the loop return the variable, which should contain the lowest number
-       return 0;
+        int min=0;
+        for (int i =0; i<= list.length-1; i++) {
+            if (list [i]< min) {
+              min = list [i];
+            }
+          }
+        return min;
     }
     
     public int max(int[] list) {
