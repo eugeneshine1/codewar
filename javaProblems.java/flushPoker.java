fab085db6dc6ccc8d321febe8cd78047f0139f1e
Eugene Shin <eugeneshine1@gmail.com>
2022-12-14 12:19:38 -0500

    pseudo code

---
+++ javaProblems.java/flushPoker.java
@@ -0,0 +1,11 @@
+class Kata{
+    public static boolean CheckIfFlush(String[] cards){
+    //set int variables for each suit
+    //loop through the array 
+    //add 1 to each suit variable when the respective suit it found in the element
+    //if any of the suit variable is the same length as the array return true 
+    //(all of the array being the same suit is a flush therefore the count variable should equal the length of array)
+    //else return false 
+    
+    }
+  }
