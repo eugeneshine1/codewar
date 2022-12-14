760a4df9f929136d54d987744c857c5c3fb2bbf6
Eugene Shin <eugeneshine1@gmail.com>
2022-12-14 12:23:35 -0500

    for each loop to count suit
    .contains to check the suit type

--- javaProblems.java/flushPoker.java
+++ javaProblems.java/flushPoker.java
@@ -6,6 +6,29 @@ class Kata{
     //if any of the suit variable is the same length as the array return true 
     //(all of the array being the same suit is a flush therefore the count variable should equal the length of array)
     //else return false 
+    int countHeart =0;
+    int countSpade =0;
+    int countDiamond =0;
+    int countClover =0;
     
+    for (String string : cards){
+      if (string.contains("H")){
+        countHeart++;
+        }
+      else if (string.contains("S")){
+        countSpade++;
+        }
+      else if (string.contains("D")){
+        countDiamond++;
+        }
+      else if (string.contains("C")){
+        countClover++;
+        }
+    }
+    if (countHeart == cards.length || countSpade == cards.length || countDiamond == cards.length ||countClover == cards.length){
+      return true;
+    } else{
+      return false;
     }
   }
+}
